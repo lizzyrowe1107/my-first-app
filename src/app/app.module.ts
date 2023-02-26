aeeaa18f0e3a6eb6cc40aea944a6fd47ad0f8c03
Liz Rowe <83372467+lizzyrowe1107@users.noreply.github.com>
2023-02-26 15:53:11 -0800

    Syncfusion Component Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SpreadsheetAllModule } from '@syncfusion/ej2-angular-spreadsheet';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SpreadsheetAllModule
   ],
   providers: [],
   bootstrap: [AppComponent]
